e4cc7e5db428aa142393d43af0dc7e11214c33aa
avdglm <arvidgilljam@gmail.com>
2022-06-19 14:19:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      <div className="category-container">
+        <img src="" />
+        <div className="category-body-builder">
+          <h2>hats</h2>
+          <p>shop now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        <img src="" />
+        <div className="category-body-builder">
+          <h2>jackets</h2>
+          <p>shop now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        <img src="" />
+        <div className="category-body-builder">
+          <h2>sneakers</h2>
+          <p>shop now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        <img src="" />
+        <div className="category-body-builder">
+          <h2>womens</h2>
+          <p>shop now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        <img src="" />
+        <div className="category-body-builder">
+          <h2>mens</h2>
+          <p>shop now</p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
